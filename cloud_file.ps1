1f78f1c5eda2f0bb3c40a82baaf7d5df5beac609
amir ghaemi <107634739+RavenRootkit@users.noreply.github.com>
2024-09-10 13:19:54 +0330

    Update cloud_file.ps1

--- cloud_file.ps1
+++ cloud_file.ps1
@@ -1,4 +1,1 @@
-$command = "Write-Host 'My voice is my passport, verify me.'" 
-$bytes = [System.Text.Encoding]::Unicode.GetBytes($command) 
-$encodedCommand = [Convert]::ToBase64String($bytes) 
-powershell.exe -EncodedCommand $encodedCommand
+Write-Host 'hi , command executed'
